2117590db3f307e23303a442ff908e6d7a0755cd
Dan Jensen <dj@fitbird.com>
2015-05-21 10:49:07 -0400

    rename components to fitbird_react

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@
 //= require angular-ui-router
 //= require angular-ui-utils
 //= require ng-token-auth
-//= require fitbird
+//= require fitbird_angular
 //= require react
 //= require react_ujs
-//= require components
+//= require fitbird_react
