f4a2466387769aa69e769501354ed77ab75e164e
Dan Jensen <dj@fitbird.com>
2015-05-24 09:40:58 -0400

    rename folder name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,6 @@
 //= require ng-token-auth
 //= require react
 //= require react_ujs
-//= require fitbird_angular
-//= require fitbird_react
+//= require _angular
+//= require _react
 //= require main.bundle
